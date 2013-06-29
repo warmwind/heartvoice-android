388eb07924b69aa8da93d9cb8f9f9dcda6b39769
Peng Jiang <pengj0520@gmail.com>
2013-06-29 17:13:49 +0800

    init project structure

---
+++ src/com/thoughtworks/MainView.java
@@ -0,0 +1,35 @@
+package com.thoughtworks;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+public class MainView extends LinearLayout {
+    EditText editText;
+    Button talkButton;
+
+    public MainView(Context context) {
+        super(context);
+        String infService = Context.LAYOUT_INFLATER_SERVICE;
+        LayoutInflater li = (LayoutInflater) getContext().getSystemService(infService);
+        li.inflate(R.layout.main, this, true);
+        editText = (EditText)findViewById(R.id.word);
+        talkButton = (Button)findViewById(R.id.talk_button);
+
+        talkButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editText.setText("clicked");
+            }
+        });
+    }
+
+    public MainView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
