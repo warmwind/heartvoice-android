3cc90829117ce04155c28a24fae6c8a2f8566d10
Ying Fu <saharaying@gmail.com>
2013-07-11 19:13:12 +0800

    #9 settings page framework

--- src/com/thoughtworks/MyActivity.java
+++ src/com/thoughtworks/MyActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 
 public class MyActivity extends Activity {
     private static final int EXPRESSIONS = 1;
+    private static final int SETTINGS = 2;
     private MainView mainView;
 
     /**
@@ -30,13 +31,16 @@ public class MyActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
+        Intent i;
         switch (item.getItemId()) {
             case R.id.useful_expressions:
-                Intent i = new Intent(this.getApplicationContext(), UsefulExpressionListActivity.class);
+                i = new Intent(this.getApplicationContext(), UsefulExpressionListActivity.class);
                 startActivityForResult(i, EXPRESSIONS);
                 return true;
-
+            case R.id.settings:
+                i = new Intent(this.getApplicationContext(), SettingsActivity.class);
+                startActivityForResult(i, SETTINGS);
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -45,14 +49,13 @@ public class MyActivity extends Activity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch(requestCode) {
-            case (EXPRESSIONS) : {
+        switch (requestCode) {
+            case (EXPRESSIONS):
                 if (resultCode == Activity.RESULT_OK) {
                     String newText = data.getStringExtra(UsefulExpressionListActivity.EXPRESSION_VALUE);
                     mainView.editText.setText(newText);
                 }
                 break;
-            }
         }
     }
 
